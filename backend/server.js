eb87f246dde0e0d848aea6c5c562df5618006ffe
unknown <printfsergelen@gmail.com>
2024-06-16 21:42:01 +0900

    db-connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require("express");
+const mongoose = require("mongoose")
 const workoutRoutes = require('./routes/workouts')
 
 const app = express()
@@ -16,8 +17,17 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000', process.env.PORT)
-})
+// db connect
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port ', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
 
