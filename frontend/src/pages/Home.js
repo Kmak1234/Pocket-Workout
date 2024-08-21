f8db7165a475eaa6eb5e1b7d8c48a90be14ecdc1
Otdev-hash <sergelen2704@gmail.com>
2024-08-21 20:02:43 +0900

    context and reducer

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
-import {useEffect, useState} from 'react'
+import {useEffect} from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
 //components
 import WorkoutDetails from '../components/WorkoutDetails'
 
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkots = async () => {
@@ -12,7 +14,7 @@ const Home = () => {
             const json = await response.json()
 
             if(response.ok){
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
 
